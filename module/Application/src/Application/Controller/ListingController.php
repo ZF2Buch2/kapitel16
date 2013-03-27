0049cf4eb04c65f200cb3f170a581461724fafd1
Ralf Eggert <ralf@travello.com>
2013-03-27 18:29:26 +0100

    Projektdateien bereit gestellt

---
+++ module/Application/src/Application/Controller/ListingController.php
@@ -0,0 +1,319 @@
+<?php
+/**
+ * ZF2 Buch Kapitel 16
+ * 
+ * Das Buch "Zend Framework 2 - Von den Grundlagen bis zur fertigen Anwendung"
+ * von Ralf Eggert ist im Addison-Wesley Verlag erschienen. 
+ * ISBN 978-3-8273-2994-3
+ * 
+ * @package    Application
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Alle Listings sind urheberrechtlich geschützt!
+ * @link       http://www.zendframeworkbuch.de/ und http://www.awl.de/2994
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Listing controller
+ * 
+ * Shows and executes the listings
+ * 
+ * @package    Application
+ */
+class ListingController extends AbstractActionController
+{
+    /**
+     * Redirects to homepage
+     */
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.01
+     */
+    public function listing1601Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.02
+     */
+    public function listing1602Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.03
+     */
+    public function listing1603Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.04
+     */
+    public function listing1604Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.05
+     */
+    public function listing1605Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.06
+     */
+    public function listing1606Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.07
+     */
+    public function listing1607Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.08
+     */
+    public function listing1608Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.09
+     */
+    public function listing1609Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.10
+     */
+    public function listing1610Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.11
+     */
+    public function listing1611Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.12
+     */
+    public function listing1612Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.13
+     */
+    public function listing1613Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 16.14
+     */
+    public function listing1614Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.15
+     */
+    public function listing1615Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.16
+     */
+    public function listing1616Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.17
+     */
+    public function listing1617Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.18
+     */
+    public function listing1618Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.19
+     */
+    public function listing1619Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.20
+     */
+    public function listing1620Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.21
+     */
+    public function listing1621Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.22
+     */
+    public function listing1622Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.23
+     */
+    public function listing1623Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.24
+     */
+    public function listing1624Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.25
+     */
+    public function listing1625Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.26
+     */
+    public function listing1626Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.27
+     */
+    public function listing1627Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.28
+     */
+    public function listing1628Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.29
+     */
+    public function listing1629Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.30
+     */
+    public function listing1630Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.31
+     */
+    public function listing1631Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.32
+     */
+    public function listing1632Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.33
+     */
+    public function listing1633Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.34
+     */
+    public function listing1634Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 16.35
+     */
+    public function listing1635Action()
+    {
+        return new ViewModel();
+    }
+}
---
+++ module/Blog/Module.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ZF2 Buch Kapitel 16
+ * 
+ * Das Buch "Zend Framework 2 - Von den Grundlagen bis zur fertigen Anwendung"
+ * von Ralf Eggert ist im Addison-Wesley Verlag erschienen. 
+ * ISBN 978-3-8273-2994-3
+ * 
+ * @package    Blog
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Alle Listings sind urheberrechtlich geschützt!
+ * @link       http://www.zendframeworkbuch.de/ und http://www.awl.de/2994
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Blog;
+
+/**
+ * Blog module class
+ * 
+ * @package    Blog
+ */
+class Module {
+}
